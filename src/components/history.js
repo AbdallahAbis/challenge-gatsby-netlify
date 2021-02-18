5ac2c0beaee6600d64f8e0f113207ebcca4dcde0
Abdallah Abis <abis.abdallah@gmail.com>
2021-02-18 15:39:32 +0100

    ✨  Added History Page and Post

---
+++ src/components/history.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, graphql, StaticQuery } from 'gatsby';
+
+const History = ({ data }) => {
+	const { edges: posts } = data.allMarkdownRemark;
+	return (
+		<div className='columns is-multiline'>
+			{posts &&
+				posts.map(({ node: post }) => (
+					<div className='is-parent column is-6' key={post.id}>
+						<article
+							className={`blog-list-item tile is-child box notification`}
+						>
+							<header>
+								<p className='post-meta'>
+									<Link
+										className='title has-text-primary is-size-4'
+										to={post.fields.slug}
+									>
+										{post.frontmatter.title}
+									</Link>
+								</p>
+							</header>
+							<p>
+								{post.excerpt}
+								<br />
+								<br />
+								<Link className='button' to={post.fields.slug}>
+									Keep Reading →
+								</Link>
+							</p>
+						</article>
+					</div>
+				))}
+		</div>
+	);
+};
+
+History.propTypes = {
+	data: PropTypes.shape({
+		allMarkdownRemark: PropTypes.shape({
+			edges: PropTypes.array,
+		}),
+	}),
+};
+
+export default () => (
+	<StaticQuery
+		query={graphql`
+			query HistoryRollQuery {
+				allMarkdownRemark(
+					filter: { frontmatter: { templateKey: { eq: "history-page" } } }
+				) {
+					edges {
+						node {
+							excerpt(pruneLength: 300)
+							id
+							fields {
+								slug
+							}
+							frontmatter {
+								title
+							}
+						}
+					}
+				}
+			}
+		`}
+		render={(data, count) => <History data={data} count={count} />}
+	/>
+);
